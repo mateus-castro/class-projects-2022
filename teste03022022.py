9fe9e400f2189efe2c3ba56a73798ff3fb48cd2d
Mateus Castro <mateus.fortes@bandtec.com.br>
2022-02-04 18:23:33 -0300

    [Análise de ALgoritmos] criando arquivo de testes

---
+++ teste03022022.py
@@ -0,0 +1,15 @@
+# d='teste'
+# print(f"id do objeto d -> {id(d)}")
+# print(f"id HEX do objeto d -> {hex(id(d))}")
+# d=213
+# print(f"id do objeto d depois de ser sobrescrito -> {id(d)}")
+# l=d
+# print(f"id do objeto l depois de ter o objeto d atribuido à ele -> {id(l)}")
+
+testString="meu nome é mateus castrooooooo"
+
+print(testString [11:17]) # pega a string dos caracteres 11 ao 17
+print(testString [18:25])
+print(testString[::]) # se refere à string inteira
+print(f"{testString[6:10:8]}") # tendi não, dps tem que ver
+print(len(testString)) # tamanho da string
