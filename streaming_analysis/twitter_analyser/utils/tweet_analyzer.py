490a13011c03e3d7435770ae69b5be8ac1250ec9
Mateus Castro <mateus.fortes@bandtec.com.br>
2022-06-07 19:17:46 -0300

    WIP: atualizando scripts de banco

--- streaming_analysis/twitter_analyser/utils/tweet_analyzer.py
+++ streaming_analysis/twitter_analyser/utils/tweet_analyzer.py
@@ -2,5 +2,13 @@ class TweetAnalyzer():
     def __init__(self, bDconnection=''):
         self.connection = bDconnection
 
-    def main():
-        print('xablau')
+    def main(self):
+        cursor = self.connection.cursor()
+
+        selectQuery = f"SELECT ID FROM USER WHERE {whereSyntax} LIMIT {numberOfStreamings};"
+        print(selectQuery)
+        cursor.execute(selectQuery)
+
+        # se o registro praquele filme ainda não tiver sido criado na tabela de análise, criar. se não criar registro
+        userQueryResult = cursor.fetchall()
+        cursor.close()
