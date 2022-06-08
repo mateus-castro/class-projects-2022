76e6be457df136e74042aacb437ac44d9952f594
Mateus Castro <mateus.fortes@bandtec.com.br>
2022-06-08 17:18:30 -0300

    Feat: analytics

--- streaming_analysis/azure_database.sql
+++ streaming_analysis/azure_database.sql
@@ -1,8 +1,11 @@
 CREATE TABLE RATE_ANALYSIS (
 	ID INT IDENTITY(1,1) PRIMARY KEY,
-	CONTENT VARCHAR(100),
+	MEDIA_CONTENT VARCHAR(100),
 	IMPRESSIONS VARCHAR(100),
 	POSITIVE INT,
 	NEGATIVE INT,
 	NEUTRAL INT
 );
+
+INSERT INTO RATE_ANALYSIS (MEDIA_CONTENT, IMPRESSIONS, POSITIVE, NEGATIVE, NEUTRAL) VALUES  ('Dick Johnson Is Dead', 'good, bad, awful, nice, pretty', 102, 115, 204),
+                                                                                            ('Blood & Water', 'much, good, gross, nice, wow', 102, 115, 204);
