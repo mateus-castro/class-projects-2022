8c149e174860727530d49eb3225f5ba4be42d89c
Mateus Castro <mateus.fortes@bandtec.com.br>
2022-06-08 17:18:52 -0300

    Feat: insert azure

--- streaming_analysis/azure_database.sql
+++ streaming_analysis/azure_database.sql
@@ -7,5 +7,14 @@ CREATE TABLE RATE_ANALYSIS (
 	NEUTRAL INT
 );
 
-INSERT INTO RATE_ANALYSIS (MEDIA_CONTENT, IMPRESSIONS, POSITIVE, NEGATIVE, NEUTRAL) VALUES  ('Dick Johnson Is Dead', 'good, bad, awful, nice, pretty', 102, 115, 204),
-                                                                                            ('Blood & Water', 'much, good, gross, nice, wow', 102, 115, 204);
+INSERT INTO RATE_ANALYSIS (MEDIA_CONTENT, IMPRESSIONS, POSITIVE, NEGATIVE, NEUTRAL) VALUES  ('Dick Johnson Is Dead', 'good', 102, 115, 204),
+                                                                                            ('Blood Water', 'much', 68, 56, 189),
+                                                                                            ('Ganglands', 'fine', 73, 58, 209),
+                                                                                            ('Stranger Things', 'wonderful', 376, 98, 539),
+                                                                                            ('Dark', 'good', 208, 117, 480),
+																							('Pretty Little Liars', 'boring', 180, 187, 436),
+																							('Percy Jackson', 'good', 166, 129, 453),
+																							('Monsters', 'good', 124, 78, 267),
+																							('Monsters', 'fine', 149, 178, 337),
+                                                                                            ('Stranger Things', 'good', 490, 132, 639),
+                                                                                            ('Stranger Things', 'perfect', 478, 124, 616);
